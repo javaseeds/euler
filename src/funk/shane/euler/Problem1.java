d981cb82150ba92543f9fc35888e3f6d6d2bd2bb
Shane Funk <michaelshanefunk@gmail.com>
2015-03-04 19:34:56 -0600

    new update

--- src/funk/shane/euler/Problem1.java
+++ src/funk/shane/euler/Problem1.java
@@ -4,11 +4,15 @@ package funk.shane.euler;
  * Created by Shane on 12/31/2014.
  */
 public class Problem1 {
+
     public int sum(int num) {
 
-        return sum(num--);
-//        int x = 0;
-//
+        if(num < 1 || (num%3 == 0 || num%5 == 0)) {
+            return 0;
+        }
+        else {
+            return num + sum(num--);
+        }
 //        for(int i = 0; i < upper; i++) {
 //            if(i%3 == 0) {
 //                x += i;
@@ -18,7 +22,6 @@ public class Problem1 {
 //            }
 //        }
 //
-//        return x;
     }
 
     public static void main(String[] args) {
